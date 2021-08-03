8499ff751ca03afc98a87ef4ddcf23315feb34f3
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-08-03 10:30:03 -0300

    Componente: Summary

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { Header } from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
