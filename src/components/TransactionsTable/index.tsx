2838ec4bd12352a6f9d8515e58f822975ac8f240
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-08-23 10:36:22 -0300

    Calculando resumo

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -22,9 +22,10 @@ export function TransactionsTable() {
                 {transactions.map(transaction => (
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
-                            <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
-                                style: 'currency',
-                                currency: 'BRL'
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
                             }).format(transaction.amount)}</td>
                             <td>{transaction.category}</td>
                             <td>
